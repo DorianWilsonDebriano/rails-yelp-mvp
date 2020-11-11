91f35e0653c675004d7bc490a6268e392e4e1e09
Dorian Wilson-Debriano <dw.debriano@gmail.com>
2020-11-11 15:38:42 +0100

    mini yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+["chinese", "italian", "japanese", "french", "belgian"]
+
+Restaurant.create(name: "Little Saigon", address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "chinese")
+Restaurant.create(name: "Venice pizza", address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "italian")
+Restaurant.create(name: "Osaka me", address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "japanese")
+Restaurant.create(name: "La bonne franquette", address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "french")
+Restaurant.create(name: "The true fries", address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "belgian")
